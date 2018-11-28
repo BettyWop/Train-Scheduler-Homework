dcd8f7f107e831cea5643dcbf0e0d19c8eb7fa29
BettyWop <annettekalla1@gmail.com>
2018-11-27 20:29:11 -0800

    finalhomework submission

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Train-Scheduler-Homework
+# Train-Scheduler-Homework (Rules)
+
+Type in a train station, location, frequency and time to find out about the train.
+You may type in all different locations and train stations to see the differnece.
---
+++ assets/javascript/train.js
@@ -0,0 +1,59 @@
+var database = firebase.database();
+$("#add-train-btn").on("click", function (event) {
+    event.preventDefault();
+
+    var trainName = $("#inputTrain").val().trim();
+    var trainDestination = $("#inputDestination").val().trim();
+    var trainTime = moment($("#inputFirstTrainTime").val().trim(), "HH:mm").format("X");
+    var trainFrequency = $("#inputFrequency").val().trim();
+
+    var newTrain = {
+        tName: trainName,
+        tDestination: trainDestination,
+        tTime: trainTime,
+        tFrequency: trainFrequency,
+    };
+
+    database.ref().push(newTrain);
+
+    $("#inputTrain").val("");
+    $("#inputDestination").val("");
+    $("#inputFirstTrainTime").val("");
+    $("#inputFrequency").val("");
+
+
+    database.ref().on("child_added", function (childSnapshot) {
+        console.log(childSnapshot.val());
+
+        var trainName = childSnapshot.val().tName;
+        var trainDestination = childSnapshot.val().tDestination;
+        var trainTime = childSnapshot.val().tTime;
+        var trainFrequency = childSnapshot.val().tFrequency;
+
+        console.log(trainName);
+        console.log(trainDestination);
+        console.log(trainTime);
+        console.log(trainFrequency);
+    })
+    var newRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(trainDestination),
+        $("<td>").text(trainTime),
+        $("<td>").text(trainFrequency),
+    );
+
+    $("#train-table > tbody").append(newRow);
+});
+
+//dont really understand the "moment" portion and how to get the minutes away. Stuck here :/
+var randomFormat = "HH:mm";
+var convertedTime = moment(randomFormat);
+
+var hours = parseInt(diff.asHours());
+
+hours = hours - days * 24;
+
+var minutes = parseInt(diff.asMinutes());
+
+minutes = minutes - (days * 24 * 60 + hours * 60);
+
